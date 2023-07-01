2d72d3a402fc1193db61e41d5dfa23aa4b127543
taylersala <taylersala@gmail.com>
2023-07-01 12:36:27 -0700

    finished lecture

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Item = props => {
   return (
-    <div className={`item${props.item.purchased ? ' purchased' : ''}`}>
+    <div className={`item${props.item.purchased ? ' purchased' : ''}`} onClick={ () => props.toggleItem(props.item.id)}>
       <p>{props.item.name}</p>
     </div>
   );
 };
 
 export default Item;
+
--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -2,18 +2,31 @@ import React from "react";
 
 class ListForm extends React.Component {
   // Constructor with state
+  
+  constructor() {
+    super();
+    this.state = {
+      item: ''
+    }
+  }
 
   handleChanges = e => {
     // update state with each keystroke
+    e.preventDefualt();
+    this.setState({...this.state, item: e.target.value})
   };
 
   // class property to submit form
+  submitForm = e => {
+    e.preventDefault();
+    this.props.addItem(e, this.state.item);
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}> 
         {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
-        <input type="text" name="item" />
+        <input type="text" name="item" value={this.state.item} onChange={this.handleChanges}/>
         <button>Add</button>
       </form>
     );
@@ -21,3 +34,4 @@ class ListForm extends React.Component {
 }
 
 export default ListForm;
+
